26247f8f7e21d6ce55792f5edde5d293bb6bc802
rohit <rohit@050996@gmail.com>
2026-09-22 01:37:39 +0530

    project setup

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="openai/gpt-oss-120b",
+)
+
+print(chat_completion.choices[0].message.content)
